fac3e57d3e259ca14196b177e3b5a427261dd77e
Siddharth <43311132+lens7@users.noreply.github.com>
2021-01-23 23:22:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ from pyspark import SparkContext
 spark = SparkSession.builder.appName('spark-gcs-demo').config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop2-latest.jar").getOrCreate()
 
 
-#notesnew = spark.read.option("header", "false").csv("gs://mimic-db/notesnew.csv")
-notesnew = spark.read.option("header", "True").csv("gs://test-245/air.csv", header=True, inferSchema=True)
-notesnew.show()
+notesnew = spark.read.option("header", "false").csv("gs://bucketname/foldername/filename") #full address your file is needed here
+notesnew.show() # prints the dataframe
 print("\n Done Reading data in spark from a file in bucket ")
-notesnew.write.option("header",True).csv("gs://test-245/air_quality")
+notesnew.write.option("header",True).csv("gs://destination_bucket/destination_folder") #the file will be saved as part files being hadoop based system
 print("\n Done writing to gcs bucket")
