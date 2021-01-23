f5e90e72d3f15cccc5b89f44d8a0e45d688a1a25
Siddharth <43311132+lens7@users.noreply.github.com>
2021-01-23 23:27:15 +0530

    Update app4.py

--- app4.py
+++ app4.py
@@ -4,15 +4,15 @@ import google.cloud.bigquery
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 
-#spark = SparkSession.builder.appName('spark-bigquery-demo').getOrCreate()
+#spark = SparkSession.builder.appName('spark-bigquery-demo').getOrCreate() #this is normal session creator
 spark = SparkSession.builder.appName('spark-gcs-bq-demo').config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop2-latest.jar,http://storage.googleapis.com/spark-lib/bigquery/spark-bigquery-latest_2.12.jar").getOrCreate()
-#sc = SparkContext('local', 'test')
+#the above one has got the connectors and configuration established
 
 
-bucket = "test-245"
+bucket = "temporary_bucket_name" # used by bigquery read/write operations as temporary middle place 
 spark.conf.set('temporaryGcsBucket', bucket)
 
-words = spark.read.format('bigquery').option('table', 'coe-foundation-215839:mmic_data.notes').load()
+words = spark.read.format('bigquery').option('table', 'project_name:dataset_name.table_name').load() #change the bigquery id here
 words.createOrReplaceTempView('words')
 
 word_count = spark.sql('SELECT * FROM words limit 10')
@@ -22,22 +22,12 @@ word_count = spark.sql('SELECT * FROM words limit 10')
 
 word_count.show()
 
-#notesnew = spark.read.option("header", "false").csv("gs://mimic-db/notesnew.csv")
-notesnew = spark.read.option("header", "True").csv("gs://test-245/air.csv", header=True, inferSchema=True)
+notesnew = spark.read.option("header", "false").csv("gs://bucket_name/folder_name/note.csv")
 notesnew.show()
-print("\n Read data in spark from a file in bucket and adding desired columns names \n")
+print("\n Read data in spark from a file in bucket\n")
 
-#notesnew.write.option("header",True).csv("gs://blue-prints-as-code/siddharth-backup/")
+notesnew.write.option("header",True).csv("gs://blue-prints-as-code/siddharth-backup/")
+print("\n Done writing")
 #notesnew.write.format("csv").save("gs://blue-prints-as-code/siddharth-backup/mytable")
 #notesnew.write.format("csv").option("path", "gs://blue-prints-as-code/siddharth-backup/my_table").save()
 #notesnew.coalesce(1).write.format('csv').save('gs://blue-prints-as-code/siddharth-backup/my_table/my.csv',header = 'true')
-
-'''
-gcs_bucket = 'blue-prints-as-code'
-
-gcs_filepath = 'gs://{}/my_table/hourly_bids.csv'.format(gcs_bucket)
-
-notesnew.coalesce(1).write \
-  .mode('overwrite') \
-  .csv(gcs_filepath)
-'''
